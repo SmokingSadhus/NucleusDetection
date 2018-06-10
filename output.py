0b5b39ff036d47cfbe71b9761de1c276228c7767
SmokingSadhus <amedhek@ncsu.edu>
2018-06-09 17:00:56 -0700

    changed iteration order for matching o/p format

--- output.py
+++ output.py
@@ -52,17 +52,18 @@ for subdir in os.listdir(rootdir):
     o_p = nucleus_model.predict(tst_img)
     o_p_img = o_p[1,:,:,:]
     retrieved_img = np.zeros((img_shape,img_shape,3))
-    for i in range (o_p_img.shape[0]):
-        for j in range(o_p_img.shape[1]):
+    #changed iterating order to match submission format
+    for j in range (o_p_img.shape[1]):
+        for i in range(o_p_img.shape[0]):
             if o_p_img[i][j][0]  > 0.5:
                 row_c = o_p_img[i][j][1]
                 col_c = o_p_img[i][j][2]
                 h = o_p_img[i][j][3]
                 w = o_p_img[i][j][4]
-                h_actual = h * box_size
-                w_actual = w * box_size
-                row_c = row_c * box_size/box_scale
-                col_c = col_c * box_size/box_scale
+                h_actual = h * b_box_size
+                w_actual = w * b_box_size
+                row_c = row_c * b_box_size/box_scale
+                col_c = col_c * b_box_size/box_scale
                 row_c = row_c + (i * b_box_size)
                 col_c = col_c + (j * b_box_size)
                 r_top = row_c - h_actual/2
@@ -77,17 +78,3 @@ for subdir in os.listdir(rootdir):
     retrieved_img_conv_To_img_form = Image.fromarray(retrieved_img)
     orig_image = retrieved_img_conv_To_img_form.resize((old_width,old_height))
     orig_image_arr = np.array(orig_image, dtype='float32')
-    
-      
-            
-
-    #trn_imgs.append
-    mask_files = []
-    for mf in os.listdir(rootdir_2 + '\\' + mask_dir):
-        mask_files.append(rootdir_2 + '\\' + mask_dir + '\\' + mf)
-    for mask in mask_files:
-        #print(mask)
-        image_data = preprocess_mask(mask,model_image_size = (img_shape, img_shape))
-        row,col,row_c,col_c,h,w = get_bounding_boxes(image_data , b_box_size)
-        mask_op[img_count, row , col ,:] = (1,row_c,col_c,h,w)
-    img_count = img_count + 1
